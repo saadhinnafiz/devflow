ea15a2d5349cdac3a7c07c21b1b80c7e2115c3a1
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-26 14:27:20 +0500

    setup Prettier, Tailwind plugin, and VS Code settings

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ const Page = () => {
   return (
     <div>
       <h1 className="text-3xl">Hello World</h1>
+      <p>This is a paragraph</p>
+      <ul>
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+        <li>Testing</li>
+      </ul>
     </div>
   );
 };
