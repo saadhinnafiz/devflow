3eec4bb8ceed24f0b43363ab717d8d33b34f990e
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-30 16:00:23 +0500

    feat: update NavigationLinks for dynamic profile routing and rename jobs route; add new pages for Questions, Collections, Community, and Tags

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,3 @@
+export default function QuestionsPage() {
+  return <h1>Questions Page</h1>;
+}
--- components/navigation/navbar/NavigationLinks.tsx
+++ components/navigation/navbar/NavigationLinks.tsx
@@ -5,14 +5,23 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { SheetClose } from "@/components/ui/sheet";
+import React from "react";
 
 export default function NavigationLinks({ isMobileNav = false }: { isMobileNav?: boolean }) {
   const pathname = usePathname();
+  //TODO: REMOVE Dummy ID below and replace with actual user ID.
+  const userId = "123";
 
   return (
     <>
       {sidebarLinks.map((item) => {
         const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname === item.route;
+        if (item.route === "/profile") {
+          if (userId) item.route = `${item.route}/${userId}`;
+          else return null;
+        }
+
         const LinkComponent = (
           <Link
             href={item.route}
@@ -29,11 +38,15 @@ export default function NavigationLinks({ isMobileNav = false }: { isMobileNav?:
               height={20}
               className={cn({ "invert-colors": !isActive })}
             />
-            <p className={cn(isActive ? "base-bold" : "base-medium", !isMobileNav && "max-lg:hidden")}>{item.label} </p>
+            <p className={cn(isActive ? "base-bold" : "base-medium", !isMobileNav && "max-lg:hidden")}>{item.label}</p>
           </Link>
         );
 
-        return LinkComponent;
+        return isMobileNav ? (
+          <SheetClose key={item.route} nativeButton={false} render={LinkComponent} />
+        ) : (
+          <React.Fragment key={item.route}>{LinkComponent} </React.Fragment>
+        );
       })}
     </>
   );
