1bfad79c85b1840db7458918e2e196bc9c24ec5d
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-30 12:59:20 +0500

    feat: enhance MobileNavigation with sign-in and sign-up buttons

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -5,12 +5,14 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
+  //   SheetDescription,
+  //   SheetFooter,
+  //   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import ROUTES from "@/constants/routes";
+import { Button } from "@/components/ui/button";
 
 export default function MobileNavigation() {
   return (
@@ -33,7 +35,7 @@ export default function MobileNavigation() {
 
         <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
           <SheetClose
-            className="px-5"
+            className="px-6"
             nativeButton={false}
             render={
               <section className="flex h-full flex-col gap-6 pt-16">
@@ -41,6 +43,29 @@ export default function MobileNavigation() {
               </section>
             }
           />
+
+          <div className="flex flex-col gap-3 px-6 pb-6">
+            <SheetClose
+              nativeButton={false}
+              render={
+                <Link href={ROUTES.SIGN_IN}>
+                  <Button className="small-medium btn-secondary min-h-[48px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              }
+            />
+            <SheetClose
+              nativeButton={false}
+              render={
+                <Link href={ROUTES.SIGN_UP}>
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[48px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              }
+            />
+          </div>
         </div>
       </SheetContent>
     </Sheet>
