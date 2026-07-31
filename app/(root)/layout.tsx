3f485a067394a62690a348628c1ff3b6ad56bd72
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-31 17:49:01 +0500

    feat: enhance LeftSidebar with sign-in and sign-up buttons and improve layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -12,10 +12,11 @@ export default function RootGroupLayout({
 
       <div className="flex">
         <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
       </div>
-      <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
-        <div>{children}</div>
-      </section>
     </main>
   );
 }
