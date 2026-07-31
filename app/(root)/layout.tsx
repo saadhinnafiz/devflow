6279c34e4dc24bc518689353e1f310992f3f6bdf
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-07-31 12:06:56 +0500

    feat: scaffold LeftSidebar and wire into root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar/Navbar";
 
 export default function RootGroupLayout({
@@ -6,9 +7,15 @@ export default function RootGroupLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSidebar />
+      </div>
+      <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 pb-6 max-md:pb-14 sm:px-14">
+        <div>{children}</div>
+      </section>
     </main>
   );
 }
---
+++ components/navigation/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+import NavigationLinks from "./navbar/NavigationLinks";
+
+export default function LeftSidebar() {
+  return (
+    <section className="background-light900_dark200 flex h-screen flex-col p-6">
+      <NavigationLinks />
+    </section>
+  );
+}
