7c510327b7709392a2bf9b7d1706bf5256b8d6a5
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-08-04 13:33:35 +0500

    wip: add title field to QuestionForm

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useForm } from "react-hook-form";
+import { Path, useForm } from "react-hook-form";
 import { AskQuestionSchema } from "@/lib/validations";
 import { standardSchemaResolver } from "@hookform/resolvers/standard-schema";
 import z from "zod";
+import { Field, FieldLabel } from "../ui/field";
+import { Input } from "@/components/ui/input";
 
 export default function QuestionForm() {
   const form = useForm<z.infer<typeof AskQuestionSchema>>({
@@ -15,9 +17,14 @@ export default function QuestionForm() {
     },
   });
 
+  const handleCreateQuestion = () => {};
+
   return (
-    <div>
-      <h1>question form</h1>
-    </div>
+    <form onSubmit={form.handleSubmit(handleCreateQuestion)} className="flex w-full flex-col gap-10">
+      <Field>
+        <FieldLabel htmlFor="title">Question Title</FieldLabel>
+        <Input id="title" {...form.register("title")} />
+      </Field>
+    </form>
   );
 }
