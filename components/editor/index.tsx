af8635acad9bca35d523efb692ffe87458d38f3e
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-08-06 01:39:25 +0500

    feat: add dark theme support to Editor component with custom styles

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -11,13 +11,21 @@ import {
   type MDXEditorMethods,
   type MDXEditorProps,
 } from "@mdxeditor/editor";
+import { basicDark } from "cm6-theme-basic-dark";
+import "./dark-editor.css";
+import { useTheme } from "next-themes";
 
 export default function Editor({
   editorRef,
   ...props
 }: { editorRef: ForwardedRef<MDXEditorMethods> | null } & MDXEditorProps) {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "dark" ? basicDark : [];
+
   return (
     <MDXEditor
+      key={resolvedTheme}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border"
       plugins={[
         // Example Plugin Usage
         headingsPlugin(),
