25ecb40ff9c62f4851b343d0d8abf52228658470
Saadhin <225086954+saadhinnafiz@users.noreply.github.com>
2026-08-15 09:25:48 +0500

    feat: add return type annotation to HomeFilter component and refactor filter handling logic

--- components/filter/HomeFilter.tsx
+++ components/filter/HomeFilter.tsx
@@ -1,41 +1,34 @@
 "use client";
 
-import { useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Button } from "../ui/button";
-import { useRouter, useSearchParams } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { formUrlQuery, removeKeysFromQuery } from "@/lib/url";
+import { ReactElement } from "react";
 
 const filters = [
   { name: "React", value: "react" },
   { name: "TypeScript", value: "typescript" },
-  //   { name: "newest", value: "newest" },
-  //   { name: "popular", value: "popular" },
-  //   { name: "unanswered", value: "unanswered" },
-  //   { name: "recommended", value: "recommended" },
 ];
 
-export default function HomeFilter() {
+export default function HomeFilter(): ReactElement {
+  const router = useRouter();
   const searchParams = useSearchParams();
   const filterParams = searchParams.get("filter");
-  const router = useRouter();
 
-  const [activeFilter, setActiveFilter] = useState(filterParams || "");
-
-  const handleTypeClick = (filter: string) => {
+  const handleTypeClick = (filter: string): void => {
     let newUrl = "";
-    if (filter === activeFilter) {
-      setActiveFilter("");
+
+    if (filter === filterParams) {
       newUrl = removeKeysFromQuery({
         params: searchParams.toString(),
         keysToRemove: ["filter"],
       });
     } else {
-      setActiveFilter(filter);
       newUrl = formUrlQuery({
         params: searchParams.toString(),
         key: "filter",
-        value: filter.toLowerCase(),
+        value: filter,
       });
     }
 
@@ -46,14 +39,14 @@ export default function HomeFilter() {
     <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
       {filters.map((filter) => (
         <Button
+          key={filter.name}
           onClick={() => handleTypeClick(filter.value)}
           className={cn(
             "body-medium rounded-lg px-6 py-3 capitalize shadow-none",
-            activeFilter === filter.value
+            filterParams === filter.value
               ? "bg-primary-100 text-primary-500 hover:bg-primary-100 dark:bg-dark-400 dark:text-primary-500 dark:hover:bg-dark-400"
               : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300"
           )}
-          key={filter.name}
         >
           {filter.name}
         </Button>
